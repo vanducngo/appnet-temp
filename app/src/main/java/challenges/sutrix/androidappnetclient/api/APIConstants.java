6f487145f2472552dead92dd88630af7e6bdbbf9
Ngo Van <vanducngo190@gmail.com>
2015-09-06 23:07:05 +0700

    add volley

---
+++ app/src/main/java/challenges/sutrix/androidappnetclient/api/APIConstants.java
@@ -0,0 +1,11 @@
+package challenges.sutrix.androidappnetclient.api;
+
+/**
+ * Created by ngovd on 9/5/15.
+ */
+public class APIConstants {
+    public static String WS_DOMAIN = "http://192.168.1.37/webservice/api/";
+    public static String CATEGORY = "category/";
+    public static String CATETOGY_SAMPLE = "getsample";
+    public static String API_CATEGORY_SAMPLE = WS_DOMAIN + CATEGORY+CATETOGY_SAMPLE;
+}
